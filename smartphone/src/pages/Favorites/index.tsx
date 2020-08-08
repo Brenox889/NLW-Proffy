610ee1c00f39859434ca66a596765d28c700488a
Brenox889 <brenomendes889@hotmail.com>
2020-08-08 20:23:16 -0300

    feat: Page Header Component

--- smartphone/src/pages/Favorites/index.tsx
+++ smartphone/src/pages/Favorites/index.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import { View } from 'react-native'
 
 import styles from './styles'
+import PageHeader from '../../components/PageHeader'
 
 export default function Favorites(){
-    return <View  style={styles.container}/>
+    return (
+        <View  style={styles.container}>
+            <PageHeader title="Meus Proffys favoritos"/>
+        </View>
+    )
 }
